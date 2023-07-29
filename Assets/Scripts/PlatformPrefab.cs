35f88a34459d41a983f33bd72099615ab18de0b2
Nikhil Jain <nikhil.jain@moonfroglabs.com>
2023-07-29 19:05:47 +0530

    adding new enemy platform rotating

--- Assets/Scripts/PlatformPrefab.cs
+++ Assets/Scripts/PlatformPrefab.cs
@@ -113,11 +113,11 @@ public class PlatformPrefab : MonoBehaviour
     }
 
     void activateObstacle() {
-        int obstacleType = Random.Range(0, 3);
+        int obstacleType = Random.Range(0, 4);
         CubeObstacle.SetActive(obstacleType == 0);
         CylinderObstacle.SetActive(obstacleType == 1);
         SphereObstacle.SetActive(obstacleType == 2);
-        // RotatingObstacle.SetActive(obstacleType == 3);
+        RotatingObstacle.SetActive(obstacleType == 3);
     }
 
     // Update is called once per frame
