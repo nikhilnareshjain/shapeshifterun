10b85d0297313dc70ec1dc0297e227d5c0aa9726
Nikhil Jain <nikhil.jain@moonfroglabs.com>
2023-07-29 20:42:42 +0530

    dist travelled change

--- Assets/Scripts/DistanceTraveled.cs
+++ Assets/Scripts/DistanceTraveled.cs
@@ -15,7 +15,7 @@ public class DistanceTraveled : MonoBehaviour
         // dist += Mathf.RoundToInt(distanceTraveledThisFrame * 1000) / 1000;
         Debug.Log("NJ: " + distanceTraveledThisFrame * 1000);
         dist += Mathf.RoundToInt(distanceTraveledThisFrame * 1000);
-        distanceTraveled.text = "D:" + dist / 100 + " S:" + speed;
+        distanceTraveled.text = "D:" + dist / 100;
         // Update the lastPosition to the current position for the next frame
     }
 
